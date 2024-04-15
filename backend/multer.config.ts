eb3dbb55a02b6fd81938c5b0152d2d2cc8eb2ef8
Zem-2em <124210986+Zem-2em@users.noreply.github.com>
2024-04-15 12:00:31 +0200

    Add files via upload

---
+++ backend/multer.config.ts
@@ -0,0 +1,21 @@
+import multer from 'multer'
+import path from 'path'
+import fs from 'fs'
+
+const uploadsDir = '../uploads'
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir, { recursive: true })
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadsDir)
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname))
+  },
+})
+
+const upload = multer({ storage, dest: uploadsDir })
+
+export default upload
